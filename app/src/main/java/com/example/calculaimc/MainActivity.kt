66b672f42c65501bbdb703453ca9aa12d8a05b13
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-09-17 08:30:19 -0300

    adicao dos textFields

--- app/src/main/java/com/example/calculaimc/MainActivity.kt
+++ app/src/main/java/com/example/calculaimc/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculaimc.ui.theme.CalculaImcTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +29,30 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AllScreen(modifier: Modifier = Modifier){
 
+}
+
+@Composable
+fun TextFields(
+    modifier: Modifier = Modifier,
+    peso: String,
+    altura: String,
+    onChangePeso: (String) -> Unit,
+    onChangeAltura: (String) -> Unit
+    ){
+    Column {
+        TextField(
+            value = peso,
+            onValueChange = onChangePeso
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        TextField(
+            value = altura,
+            onValueChange = onChangeAltura
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
