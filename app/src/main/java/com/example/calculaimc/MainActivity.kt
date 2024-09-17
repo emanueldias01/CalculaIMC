6967e0442384a856285957087323375c9bc622d7
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-09-17 10:36:57 -0300

    coloca os textFields na tela principal

--- app/src/main/java/com/example/calculaimc/MainActivity.kt
+++ app/src/main/java/com/example/calculaimc/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.calculaimc
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculaimc.ui.theme.CalculaImcTheme
@@ -29,6 +40,30 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AllScreen(modifier: Modifier = Modifier){
 
+    var pesoInput by remember { mutableStateOf("") }
+    var alturaInput by remember{ mutableStateOf("") }
+
+    val peso = pesoInput.toDoubleOrNull() ?: 0.0
+    val altura = alturaInput.toDoubleOrNull() ?: 0.0
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(bottom = 150.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        TextFields(peso = pesoInput,
+            altura = alturaInput,
+            onChangePeso = {pesoInput = it},
+            onChangeAltura = {alturaInput = it}
+            )
+
+
+    }
+
+
 }
 
 @Composable
@@ -40,13 +75,15 @@ fun TextFields(
     onChangeAltura: (String) -> Unit
     ){
     Column {
+        Text(text = stringResource(id = R.string.peso_txt))
         TextField(
             value = peso,
             onValueChange = onChangePeso
         )
 
-        Spacer(modifier = Modifier.height(10.dp))
+        Spacer(modifier = Modifier.height(30.dp))
 
+        Text(text = stringResource(id = R.string.altura_txt))
         TextField(
             value = altura,
             onValueChange = onChangeAltura
